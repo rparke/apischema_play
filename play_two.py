a82edfd8acec2f87b7c5ccb317b41ae25819b184
rparke <50015370+rparke@users.noreply.github.com>
2021-05-05 12:52:29 +0000

    added play_two.py

---
+++ play_two.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from typing import Union
+
+from apischema import Undefined, UndefinedType, deserialize, serialize
+from apischema.json_schema import deserialization_schema
+
+
+@dataclass
+class Foo:
+    bar: Union[int, UndefinedType] = Undefined
+    baz: Union[int, UndefinedType, None] = Undefined
